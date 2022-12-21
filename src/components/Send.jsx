f737d716cc9609b538c94b67165a5a0ad47dcd46
yash bhangale <68957369+yashbhangale@users.noreply.github.com>
2022-12-22 00:19:35 +0530

    img rmed

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react"; 
 import { TransactionContext } from "../context/TransactionContext"; 
 import { Loader } from "."; 
-import eth from "../../images/send-eth.svg";
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
@@ -29,15 +28,14 @@ const Send = () => {
   };
 
   return (
-    <div className="flex-1 flex justify-start   items-center flex-col w-full p-5">
+    <div className="flex-1 flex justify-start   items-center flex-col w-full p-5 animate-slide-in">
     <h1 className="text-white font-poppins font-black text-5xl tracking-wide">Cryptway Send</h1>
     <p className="text-gray-400	 font-poppins font-medium mt-3 text-base">Enter   Wallet Address</p>
     <div className="mt-10 w-full flex justify-center">
       <div className="relative md:max-w-[700px] md:min-w-[500px] min-w-full max-w-full gradient-border p-[2px] rounded-3xl">
-        <div className="pink_gradient" />
+        <div className="pink_gradient animate-pulse" />
         <div className="w-full  bg-[#1F1D2B] backdrop-blur-[4px] rounded-3xl shadow-card flex p-10">
            <div className="p-5   w-full flex flex-col justify-start items-center ">
-         <img src={eth}  className="w-48" alt="" />
         <Input placeholder="Address To"  className="block w-full p-4 rounded-lg   bg-gray-700  border-black  placeholder-gray-400 text-white focus:ring-blue-500  focus:border-blue-500" name="addressTo" type="text" handleChange={handleChange} />
             <Input className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full " placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
             <div className="h-[1px] w-full bg-gray-400 my-2" />
