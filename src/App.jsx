a3a9b6992e1ca8b2165667a78c3ea527df628555
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-21 19:11:19 +0530

    commit base

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar, Fund,Thank, Success, Faucet, Send, Receive, Welcome, Footer, NotFound} from "./components"; 
+ 
+function App() {
+  return (
+<Router>
+<Navbar />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/receive" element={<Receive />} /> 
+        <Route path="/send" element={<Send />} />   
+        <Route path='/swap' element={ <Swap />}/>
+        <Route path='/fund' element={ <Fund />}/>
+        <Route path='/faucet' element={ <Faucet />}/>
+        <Route path='/thanks' element={ <Thank />}/>
+        <Route path='/success' element={ <Success />}/>
+        <Route path='*' element={ <NotFound />}/>
+      </Routes>
+      <Footer />
+    </Router> 
+);
+}
+   
+  function Swap() { 
+    window.location.replace('https://cryptwayswap.netlify.app/');
+  return null;
+  }
+   
+export default App;
+
+ 
