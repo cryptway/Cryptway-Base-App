ff4d2ba8411948123192dc07eff08cc42581506f
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-03-26 22:44:52 +0530

    Update Send.jsx

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -14,7 +14,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     step="0.0001"
     value={value}
     onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    className=" "
   />
 );
 
