e8b88c4a70d757291245c1fbf787e33bd5ee0a1a
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-03-26 22:25:35 +0530

    Update Send.jsx

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -7,7 +7,7 @@ import { AiOutlineDown } from 'react-icons/ai'
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({ placeholder, name, type, value, handleChange }) => (
+const input = ({ placeholder, name, type, value, handleChange }) => (
   <input
     placeholder={placeholder}
     type={type}
